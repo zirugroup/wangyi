17d8f46b8499c8b8491925cb5b89093b1ed2cd08
wizard <sxw_1994@163.com>
2017-05-09 22:17:13 +0800

    仓库

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,11 +3,15 @@
 	    <router-link to="/">首页</router-link>
 	    <router-link to="/topic">专题</router-link>
 	    <router-link to="/car">购物车</router-link>
-	    <router-link to="/classify">分类</router-link>
+	    <router-link :to="{path :'/classify',query : {index : 0}}">分类</router-link>
 	    <router-link to="/mine">个人</router-link>
  	 </footer>
 </template>
 <script>
+	import Vue from "vue"
+	import VueResource from "vue-resource"
+    import classify from "../components/classify.vue"
+	Vue.use(VueResource)
 	export default{
 
 	}
